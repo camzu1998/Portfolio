91f4676ed6088e6e11eb01498088aa30946e83b7
camzu98 <Langer14>
2017-06-09 11:01:21 +0200

    Fontello icons

--- index.php
+++ index.php
@@ -8,11 +8,42 @@
     <link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="css/normalize.css">
     <link rel="stylesheet" href="css/skeleton.css">
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/animation.css">
+    <link rel="stylesheet" href="css/fontello.css">
 </head>
 <body>
     <div class="container">
         <div class="row">
             <div class="header"><h2>Kamil Langer</h2></div>
+        </div>
+        <div class="row">
+            <div class="four columns wtile"><!-- O mnie -->
+                <i class="icon-user"></i>
+                <h4>O mnie</h4>
+            </div>
+            <div class="four columns wtile"><!-- Moje projekty -->
+                <i class="icon-th-list"></i>
+                <h4>Moje projekty</h4>
+            </div>
+            <div class="four columns wtile"><!-- Kontakt -->
+                <i class="icon-chat"></i>
+                <h4>Kontakt</h4>
+            </div>
+        </div>
+        <div class="row"><!-- Media społecznościowe -->
+            <div class="three columns mspol">
+                <i class="icon-facebook"></i>
+            </div>
+            <div class="three columns mspol">
+                <i class="icon-github-text"></i>
+            </div>
+            <div class="three columns mspol">
+                <i class="icon-instagram"></i>
+            </div>
+            <div class="three columns mspol">
+                <i class="icon-youtube"></i>
+            </div>
         </div>
     </div>
 </body>
