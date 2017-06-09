8fca4499cd3420138bd1bf6b0f5e51579712ad39
camzu98 <Langer14>
2017-06-09 10:08:52 +0200

    Prepare resources

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Portfolio Kamil Langer</title>
+    <meta name="description" content="Portfolio młodego web developera Kamila Langer.">
+    <meta name="author" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/skeleton.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row"></div>
+    </div>
+</body>
+</html>
